b1c51ce98409832d944bd2628297273be3b86f56
trinh050103 <giavy050103@gmail.com>
2024-03-19 19:37:47 +0700

    update taikhoanController

--- Controllers/TaiKhoanController.cs
+++ Controllers/TaiKhoanController.cs
@@ -8,10 +8,42 @@ namespace LuxyryWatch.Controllers
 {
     public class TaiKhoanController : Controller
     {
-        // GET: TaiKhoan
-        public ActionResult Index()
+      // GET: TaiKhoan
+        LuxuryWatch_DB db = new LuxuryWatch_DB();
+        
+        public ActionResult ThongTinTaiKhoan()
         {
+            ThanhVien tv = (ThanhVien)Session["TaiKhoan"];
+            return View(tv);
+        }
+        [HttpPost]
+        public ActionResult ThongTinTaiKhoan(ThanhVien tv)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tv).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                ThanhVien tvn = (ThanhVien)Session["TaiKhoan"];
+                return View(tvn);
+            }
+            ViewBag.ThongBao = "Có lỗi xảy ra!";
             return View();
         }
+        //public ActionResult DoiMatKhau(string txtMKC, string txtMKM, string txtNLMK)
+        //{
+        //    string mkc = MaHoa.MD5Hash(txtMKC);
+        //    string mkm = MaHoa.MD5Hash(txtMKM);
+        //    string nlmk = MaHoa.MD5Hash(txtNLMK);
+        //    ThanhVien tv = (ThanhVien)Session["TaiKhoan"];
+        //    if (!tv.MatKhau.Contains(mkc))
+        //    {
+        //        return Content("Mật khẩu không chính xác!");
+        //    }
+        //    ThanhVien result = db.ThanhViens.Single(x => x.TaiKhoan.Contains(tv.TaiKhoan));
+        //    result.MatKhau = mkm;
+        //    db.SaveChanges();
+        //    return Content("<script>window.location.reload();</script>");
+        //}
     }
+    
 }
